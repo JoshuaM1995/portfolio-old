177e98a334e0a08bfc442911bf5fe2062d4840d9
joshuamcnabb <joshua@lc.dev>
2022-04-15 18:18:06 -0600

    change folder structure in terminal sidebar explorer

--- components/terminal/TerminalSideBar/TerminalSideBar.tsx
+++ components/terminal/TerminalSideBar/TerminalSideBar.tsx
@@ -3,7 +3,7 @@ import { hikingContent, laravelContent, listeningToMusicContent, longboardingCon
 import { videoGamesContent } from "@content/interests/free-time/video-games";
 import noContent from "@content/noContent";
 import { faLaravel, faMarkdown, faNodeJs, faReact } from "@fortawesome/free-brands-svg-icons";
-import { faChevronRight, faFolder } from "@fortawesome/free-solid-svg-icons";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AboutContext } from "context/AboutContext";
 import Tree, { TreeProps } from "rc-tree";
@@ -22,22 +22,30 @@ export const TerminalSideBar = () => {
       icon: <FontAwesomeIcon icon={faFolder} color={treeIconColor.folder} />,
       selectable: false,
       children: [
-        { key: '0-0', title: 'laravel.php', icon: <FontAwesomeIcon icon={faLaravel} color={treeIconColor.laravel} /> },
-        { key: '0-1', title: 'node.js', icon: <FontAwesomeIcon icon={faNodeJs} color={treeIconColor.nodeJS} /> },
-        { key: '0-2', title: 'React.jsx', icon: <FontAwesomeIcon icon={faReact} color={treeIconColor.react} /> },
         {
-          key: '0-3',
+          key: '0-1',
           title: 'Free Time',
           icon: <FontAwesomeIcon icon={faFolder} color={treeIconColor.folder} />,
           selectable: false,
           children: [
-            { key: '0-3-1', title: 'hiking.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
-            { key: '0-3-2', title: 'longboarding.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
-            { key: '0-3-3', title: 'listening_to_music.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
-            { key: '0-3-4', title: 'video_games.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
-            { key: '0-3-5', title: 'watching_movies.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
+            { key: '0-1-1', title: 'hiking.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
+            { key: '0-1-2', title: 'longboarding.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
+            { key: '0-1-3', title: 'listening_to_music.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
+            { key: '0-1-4', title: 'video_games.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
+            { key: '0-1-5', title: 'watching_movies.md', icon: <FontAwesomeIcon icon={faMarkdown} color={treeIconColor.markdown} /> },
           ]
         },
+        {
+          key: '0-2',
+          title: 'Programming',
+          icon: <FontAwesomeIcon icon={faFolder} color={treeIconColor.folder} />,
+          selectable: false,
+          children: [
+            { key: '0-2-1', title: 'laravel.php', icon: <FontAwesomeIcon icon={faLaravel} color={treeIconColor.laravel} /> },
+            { key: '0-2-2', title: 'node.js', icon: <FontAwesomeIcon icon={faNodeJs} color={treeIconColor.nodeJS} /> },
+            { key: '0-2-3', title: 'React.jsx', icon: <FontAwesomeIcon icon={faReact} color={treeIconColor.react} /> },
+          ]
+        }
       ],
     },
     {
@@ -66,32 +74,29 @@ export const TerminalSideBar = () => {
 
 const getContentByKey = (key: string) => {
   switch (key) {
-    case '0-0':
-      return laravelContent;
-
-    case '0-1':
-      return nodeContent;
-
-    case '0-2':
-      return reactContent;
-
-    case '0-3-1':
+    case '0-1-1':
       return hikingContent;
 
-    case '0-3-2':
+    case '0-1-2':
       return longboardingContent;
 
-    case '0-3-3':
+    case '0-1-3':
       return listeningToMusicContent;
 
-    case '0-3-4':
+    case '0-1-4':
       return videoGamesContent;
 
-    case '0-3-5':
+    case '0-1-5':
       return watchingMoviesContent;
 
-    case '1':
-      return aboutContent;
+    case '0-2-1':
+      return laravelContent;
+
+    case '0-2-2':
+      return nodeContent;
+
+    case '0-2-3':
+      return reactContent;
 
     default:
       return noContent;
