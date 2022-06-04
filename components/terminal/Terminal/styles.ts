4792f4872d6c49b95dd2e1a247a18d2b07e068cc
joshuamcnabb <joshua@lc.dev>
2022-06-03 21:18:40 -0600

    fix ts error

--- components/terminal/Terminal/styles.ts
+++ components/terminal/Terminal/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from '@styles/theme';
 
 export const TerminalContainer = styled.div`
-  color: ${({ theme: { colors } }) => colors.terminal.text};
+  color: ${({ theme: { colors } }) => colors.text};
   background: ${({ theme: { colors } }) => colors.terminal.background};
   border-radius: 6px;
 `;
